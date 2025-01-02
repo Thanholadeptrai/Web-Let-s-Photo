491b5c4fcd0e831aa9761d97924dd66bf4f1f490
Than Nguyen <thannguyen@gmail.com>
2025-01-02 13:05:31 +0700

    đẩy tcn ra ngoài

--- component/navbar/main.js
+++ component/navbar/main.js
@@ -167,33 +167,5 @@ window.addEventListener('DOMContentLoaded', (event) => {
             }
         });
     }
-    
-    const HeaderComponentPhototcn = document.querySelector('header-component-phototcn');
-    if (HeaderComponentPhototcn) {
-        const shadow = HeaderComponentPhototcn.shadowRoot;
 
-        let menu = shadow.querySelector('#menu-icon');
-        let navlist = shadow.querySelector('.navlist');
-        let scrollButton = shadow.getElementById('scroll-up');
-
-        menu.onclick = () => {
-            menu.classList.toggle('bx-x');
-            navlist.classList.toggle('open');
-        }
-
-        window.onscroll = () => {
-            menu.classList.remove('bx-x');
-            navlist.classList.remove('open');
-        }
-
-        window.addEventListener("scroll", function() {
-            if (window.scrollY > 100) {
-                scrollButton.classList.add("show-scroll");
-                scrollButton.style.display = 'block';
-            } else {
-                scrollButton.classList.remove("show-scroll");
-                scrollButton.style.display = 'none';
-            }
-        });
-    }
 });
